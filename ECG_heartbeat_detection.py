4335b3d5c53f906a52f94898cda6271038f148bd
Amin Aminifar <43568226+AminAminifar@users.noreply.github.com>
2023-05-24 16:11:04 +0200

    Add files via upload

--- ECG_heartbeat_detection.py
+++ ECG_heartbeat_detection.py
@@ -1,5 +1,9 @@
 from ecgdetectors import Detectors
+from tqdm import tqdm
+import pickle
+import pandas as pd
 
+from IPython.display import display, HTML
 
 # detect r peaks
 fs = 360  # sample freq
@@ -13,7 +17,45 @@ def r_peak_detect(in_array, algorithm):
     try:
         result = eval(string_script)  # exec(string_script)
         out_flag = True
-    except IndexError:
+    except (IndexError, ValueError):
         out_flag = False
 
     return out_flag, result
+
+
+def detect_and_save(records_df, ECG_algorithms, filename_str='r_peak_dictionary.pkl'):
+
+    df = pd.DataFrame(columns=['patient', 'channel', 'algorithm', 'out_flag', 'r_peak'])
+    for alg in ECG_algorithms:
+        print("\nalgorithm: ", alg)
+        sum_true = 0
+        sum_records = 0
+        for patient, patient_index in \
+                zip(records_df['patient'].unique(), tqdm(range(len(records_df['patient'].unique())))):
+            for channel in records_df['channel'].unique():
+                signals_index = \
+                    records_df[(records_df['patient'] == patient) &
+                                    (records_df['channel'] == channel)].index
+                signals = records_df.loc[signals_index[0]]['signals']
+                for arr in signals:
+                    out_flag, result = r_peak_detect(arr, alg)
+
+                    if not out_flag:
+                        result = []
+                    dictionary = {'patient': patient, 'channel': channel, 'algorithm': alg,
+                                  'out_flag': out_flag, 'r_peak': result}
+                    temp_df = pd.DataFrame(dictionary)
+
+                    df = pd.concat([df, temp_df], ignore_index=True)
+                    df.reset_index()
+
+                    sum_records += 1
+                    if out_flag:
+                        sum_true += 1
+        print("\npercentage of samples with no error: ", (sum_true/sum_records)*100, "%")
+        print("next algorithm...")
+
+    print("performed the r peak detection for all algorithms in the list.")
+    print("saving the results...")
+    df.to_pickle(filename_str)
+    print("results saved.")
--- Import_data.py
+++ Import_data.py
@@ -1,30 +1,36 @@
 import numpy as np
 from wfdb.io.record import rdsamp
+import pandas as pd
 
 
-def import_data():
+def import_data(noisy_flag=False, noise_str="e24"):
     # Import original signals
-    # print("records_list:")
-    records_list = []
     records = np.loadtxt("mit-bih-arrhythmia-database-1.0.0/RECORDS", dtype=int)
-    print(len(records))
+    # num_records = len(records)
+    num_channels = 2
+    records_df = pd.DataFrame(columns=['patient', 'channel', 'signals'])
     for record in records:
-        for ch in range(2):
-            sig, fields = rdsamp('mit-bih-arrhythmia-database-1.0.0/' + str(record), channels=[ch])
-            records_list = records_list + np.split(sig[:649980], 30)  # 648000
-    # print(len(records_list), np.array(records_list)[:, :, 0].shape, type(records_list))
-    # np.savetxt("records.csv", np.array(records_list)[:, :, 0], delimiter=",")
-    # with open('test.npy', 'wb') as f:
-    #     np.save(f, np.array(records_list)[:, :, 0])
-    # with open('test.npy', 'rb') as f:
-    #     a = np.load(f)
+        for ch in range(num_channels):
+            if noisy_flag:
+                sig, _ = rdsamp('mit-bih-arrhythmia-database-1.0.0/' + str(record) + noise_str, channels=[ch])
+            else:
+                sig, _ = rdsamp('mit-bih-arrhythmia-database-1.0.0/' + str(record), channels=[ch])
+            sig_f = sig.flatten()
+            # if we need the whole signal as one records:
+            # records_list.append(sig_f)
+            # (else) if we need to split the signal:
+            splitted_signals = [np.split(sig_f[:649980], 30)] # 648000
 
-    # Import noisy signals
-    # print("noisy_records_list:")
-    noisy_records_list = []
-    for record in records:
-        for ch in range(2):
-            sig, fields = rdsamp('mit-bih-arrhythmia-database-1.0.0/' + str(record) + "e24", channels=[ch])
-            noisy_records_list = noisy_records_list + np.split(sig[:649980], 30)
-    # print(len(noisy_records_list))
-    return records_list, noisy_records_list
+            dictionary = {'patient': record, 'channel': ch, 'signals': splitted_signals}
+            temp_df = pd.DataFrame(dictionary)
+            records_df = pd.concat([records_df, temp_df], ignore_index=True)
+            records_df.reset_index()
+
+    return records_df
+
+# print(len(records_list), np.array(records_list)[:, :, 0].shape, type(records_list))
+# np.savetxt("records.csv", np.array(records_list)[:, :, 0], delimiter=",")
+# with open('test.npy', 'wb') as f:
+#     np.save(f, np.array(records_list)[:, :, 0])
+# with open('test.npy', 'rb') as f:
+#     a = np.load(f)
